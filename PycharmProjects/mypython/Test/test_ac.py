d6f23201da501004befd1a2013c41f98c27ad9eb
davidojo1144 <davco1144@gmail.com>
2024-10-09 22:44:34 +0100

    tdd

--- PycharmProjects/mypython/Test/test_ac.py
+++ PycharmProjects/mypython/Test/test_ac.py
@@ -5,22 +5,42 @@ class TestAc(unittest.TestCase):
 
     def test_ac_is_off(self):
         ac = Ac()
-        ac.check_ac_is_on()
+        ac.check_ac_is_off()
 
     def test_ac_can_be_switch_on(self):
         ac = Ac()
         ac.check_ac_is_on()
-        ac = Ac()
         ac.check_ac_has_been_switched_on()
 
 
     def test_ac_is_on(self):
         ac = Ac()
         ac.check_ac_is_on()
-        ac = Ac()
         ac.check_ac_has_been_switched_on()
+        ac.check_ac_is_on()
+
+
+    def test_that_ac_temperature_increased(self):
         ac = Ac()
-        ac.check_ac_is_off()
+        ac.check_ac_is_on()
+        self.assertEqual(ac.check_ac_temperature_increased(1),2)
+
+    def test_that_ac_temperature_decreased(self):
+        ac = Ac()
+        ac.check_ac_is_on()
+        self.assertEqual(ac.check_ac_temperature_decreased(5), 4)
+
+
+    def test_that_ac_cannot_be_increased_beyond_30(self):
+        ac = Ac()
+        ac.check_ac_is_on()
+        self.assertEqual(ac.ac_temp_cannot_go_beyond_30(35),30)
+
+    def test_that_ac_cannot_be_decreased_below_16(self):
+        ac = Ac()
+        ac.check_ac_is_on()
+        self.assertEqual(ac.ac_temp_cannot_get_below_16(15),16)
+
 
 
         
--- PycharmProjects/mypython/ac.py
+++ PycharmProjects/mypython/ac.py
@@ -1,15 +1,42 @@
 class Ac:
 
     def __init__(self):
-        self.is_on = False
+        self.is_off = False
+        self.increment = 1
+        self.decrement = 1
+        self.highest_temp = 30
+        self.lowest_temp = 16
 
 
-    def check_ac_is_on(self):
-        return self.is_on
+    def check_ac_is_off(self):
+        return self.is_off
 
 
     def check_ac_has_been_switched_on(self):
-        return self.is_on == True
+        return self.is_off == True
 
-    def check_ac_is_off(self):
-        return self.is_on == True
+    def check_ac_is_on(self):
+        return self.is_off == True
+
+    def check_ac_temperature_increased(self, temperature):
+        return temperature + self.increment
+
+
+    def check_ac_temperature_decreased(self, temperature):
+        return temperature - self.decrement
+
+
+    def ac_temp_cannot_go_beyond_30(self, temperature):
+        if temperature < self.highest_temp:
+            return temperature + self.increment
+        elif temperature > self.highest_temp:
+            return self.highest_temp
+
+
+    def ac_temp_cannot_get_below_16(self, temperature):
+        if temperature > self.lowest_temp:
+            return temperature + self.increment
+        elif temperature < self.lowest_temp:
+            return self.lowest_temp
+
+
